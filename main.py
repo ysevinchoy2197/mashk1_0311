a9b43b9973236028b90ffcfd3e5342e6b126ca20
ysevinchoy2197 <ysevinchoy2197@gmail.com>
2025-11-03 17:48:34 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,84 @@
+#1-misol
+summa = 0
+for i in range(1, 11):
+    summa += i
+
+print(summa)
+
+#2-misol
+summa = 0
+for i in range(1, 51):
+    if i % 2 != 0:
+       summa += i
+
+print(summa)
+
+
+#3-misol
+summa = 0
+for i in range(1, 101):
+    if i % 2 == 0:
+        summa += i
+
+print(summa)
+
+#4-misol
+summa = 0
+for i in range(10, 31):
+    summa += i
+print(summa)
+
+
+#5-misol
+n = int(input("n ni kirit: "))
+
+summa = 0
+for i in range(1, n+1):
+    summa += i
+
+print(summa)
+
+
+#6-misol
+summa = 0
+for i in range(1, 21):
+    summa += i
+
+print(summa)
+
+
+#7-misol
+summa = 0
+for i in range(5, 25):
+    summa += i
+print(summa)
+
+
+
+#8-misol
+summa = 0
+for i in range(1, 16):
+    if i % 3 == 0:
+        summa += i
+print(summa)
+
+
+#9-misol
+summa = 0
+for i in range(50, 101):
+    if i % 2 == 0:
+        summa += i
+print(summa)
+
+
+
+#10-misol
+summa = 0
+for i in range(1, 31):
+    if i % 5 == 0:
+        summa += i
+print(summa)
+
+
+
+
